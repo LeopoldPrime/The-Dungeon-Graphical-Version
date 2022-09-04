f81bc31cb701f2ae409de38ab46b2e723173f3f4
LeopoldPrime <87269058+LeopoldPrime@users.noreply.github.com>
2022-09-04 16:27:16 -0400

    Made Some Changes To All Files

---
+++ dungeon crawler/dungeon crawler/encounters.py
@@ -0,0 +1,17 @@
+import random
+
+encounterChance = [1,2,3,4,5,6]
+randomNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+def numberGuessEncounter():
+    print("lorem ipsum")
+
+def hangManEncounter():
+    print("lorem ipsum")
+
+def encounterSelector():
+    if random.choice(encounterChance) <= 4:
+        if random.choice(randomNumbers) <= 5:
+            numberGuessEncounter()
+        else:
+            hangManEncounter()
